2a9cecca7684ecd741c5f9b9df87d9981da0b238
JimW <jim.wallace@versionone.com>
2016-11-05 14:38:50 -0400

    Better tab completion to remove dupe attribute names

--- Scripts/RegisterCompleter.ps1
+++ Scripts/RegisterCompleter.ps1
@@ -27,17 +27,25 @@ $attributeTabComplete = {
 
     if ( $global:debuggingTabV1 )
     {
-        [System.IO.File]::AppendAllText("c:\temp\registerCompleter.txt", "commandName: $commandName parameterName: $parameterName wordToComplete: $wordToComplete commandAst: $($commandAst.gettype()) fakeBoundParameter $($fakeBoundParameter |out-string)`n")
+        [System.IO.File]::AppendAllText("c:\temp\registerCompleter.txt", "commandName: $commandName parameterName: $parameterName wordToComplete: $wordToComplete commandAst: $($commandAst.commandElements | fl -fo | out-string) fakeBoundParameter $($fakeBoundParameter |out-string)`n")
     }
 
     if ( $fakeBoundParameter.keys -contains "assetType")
     {
         if ( $global:debuggingTabV1 )
         {
-            [System.IO.File]::AppendAllText("c:\temp\registerCompleter.txt", "Got key`n");
+            [System.IO.File]::AppendAllText("c:\temp\registerCompleter.txt", "Found assetType parameter before this`n");
             [System.IO.File]::AppendAllText("c:\temp\registerCompleter.txt", "$(Get-V1Meta -assetType $fakeBoundParameter['assetType'])`n");
         }
-        return (Get-V1Meta -assetType $fakeBoundParameter["assetType"] ).keys | Where-Object {$_ -like "$wordToComplete*"} | Sort-Object | ForEach-Object { [System.Management.Automation.CompletionResult]::new($_)}
+        $excludes = @()
+        [System.IO.File]::AppendAllText("c:\temp\registerCompleter.txt", "Last command element $($commandAst.commandElements[-1] | ft -fo | out-string))`n");
+        
+        if ( $commandAst.commandElements[-1].Extent )
+        {
+            $excludes = $commandAst.commandElements[-1].Extent -split ","
+        }
+        [System.IO.File]::AppendAllText("c:\temp\registerCompleter.txt", "Excluding $excludes`n");
+        return (Get-V1Meta -assetType $fakeBoundParameter["assetType"] ).keys | Where-Object {$_ -like "$wordToComplete*" -and $_ -notin $excludes } | Sort-Object | ForEach-Object { [System.Management.Automation.CompletionResult]::new($_)}
     }
     else 
     {
@@ -45,11 +53,13 @@ $attributeTabComplete = {
     }
 }
 
-Register-ArgumentCompleter -CommandName "Get-V1Asset","Get-V1AssetPaged","Get-V1Meta","Get-V1FilterAsset","Get-V1MetaAttribute","Get-V1MetaAssetType","New-V1Asset" -ParameterName "assetType" -ScriptBlock $assetTypeTabComplete
-Register-ArgumentCompleter -CommandName "Remove-V1Asset" -ParameterName "id" -ScriptBlock $assetTypeTabComplete
+# asset Type
+Register-ArgumentCompleter -CommandName "Get-V1Asset","Get-V1AssetPaged","Get-V1Meta","Get-V1FilterAsset","Get-V1MetaAttribute","Get-V1MetaAssetType","New-V1Asset" -ParameterName "AssetType" -ScriptBlock $assetTypeTabComplete
+Register-ArgumentCompleter -CommandName "Remove-V1Asset" -ParameterName "ID" -ScriptBlock $assetTypeTabComplete
 
+# attribute for functions that take assetType
 Register-ArgumentCompleter -CommandName "Get-V1Asset","Get-V1AssetPaged","New-V1Asset" -ParameterName "attributes" -ScriptBlock $attributeTabComplete
-Register-ArgumentCompleter -CommandName "New-V1Asset" -ParameterName "defaultAttributes" -ScriptBlock $attributeTabComplete
-Register-ArgumentCompleter -CommandName "Get-V1Asset","Get-V1AssetPaged" -ParameterName "sort" -ScriptBlock $attributeTabComplete
-Register-ArgumentCompleter -CommandName "Get-V1Asset","Get-V1AssetPaged" -ParameterName "filter" -ScriptBlock $attributeTabComplete
-Register-ArgumentCompleter -CommandName "Set-V1Value" -ParameterName "name" -ScriptBlock $attributeTabComplete
+Register-ArgumentCompleter -CommandName "New-V1Asset" -ParameterName "Names" -ScriptBlock $attributeTabComplete
+Register-ArgumentCompleter -CommandName "Get-V1Asset","Get-V1AssetPaged" -ParameterName "Sort" -ScriptBlock $attributeTabComplete
+Register-ArgumentCompleter -CommandName "Get-V1Asset","Get-V1AssetPaged" -ParameterName "Filter" -ScriptBlock $attributeTabComplete
+Register-ArgumentCompleter -CommandName "Set-V1Value" -ParameterName "Name" -ScriptBlock $attributeTabComplete
